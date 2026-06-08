28acdf01ec6c4f85686bc5e65f49163381337d0d
Saber Zou <saberzou@gmail.com>
2026-06-08 23:53:37 +0800

    feat: add AI podcast for The Sense of Style (2026-06-09)

--- data.js
+++ data.js
@@ -1770,6 +1770,7 @@ const dailyData = [
     },
     {
         date: "2026-06-09",
+        audio: "https://qxtynvj1amcfkfqv.public.blob.vercel-storage.com/littlebook/audio/2026-06-09.mp3",
         book: {
             isbn: "9780143127796",
             title: "The Sense of Style",
